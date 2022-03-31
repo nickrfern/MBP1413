1a27363c28df63932dbbcf6fcd8fd89fe71261e4
yuanch99 <94755261+yuanch99@users.noreply.github.com>
2022-03-31 18:06:31 -0400

    Update knn_CV.py

--- models/knn/knn_CV.py
+++ models/knn/knn_CV.py
@@ -40,24 +40,24 @@ for cv in range(4,5):
     record_metric = utils.history(knn,X_train,y_train,X_validation,y_validation,k,matrix)
     records = records.append(record_metric,ignore_index=True)
 
-    # ####save model
-    # if i == 0:
-    #   time_used=end-start
-    #   acc = np.mean(record_k['val_acc'])
-    #   variabels=k
-    #   filename = f'{OUTPUT}knn_model.sav'
-    #   pickle.dump(knn, open(filename, 'wb'))
-    # elif np.mean(record_k['val_acc'])>acc:
-    #   variabels=k
-    #   time_used=end-start
-    #   pickle.dump(knn, open(filename, 'wb'))
-    #   print('dumped')
-    # ##########
+    ####save model
+    if i == 0:
+      time_used=end-start
+      acc = np.mean(record_k['val_acc'])
+      variabels=k
+      filename = f'{OUTPUT}knn_model.sav'
+      pickle.dump(knn, open(filename, 'wb'))
+    elif np.mean(record_k['val_acc'])>acc:
+      variabels=k
+      time_used=end-start
+      pickle.dump(knn, open(filename, 'wb'))
+      print('dumped')
+    ##########
 
-  # ##final Model test accuracies
-  # loaded_model = pickle.load(open(filename, 'rb'))
-  # matrix = confusion_matrix(y_test,loaded_model.predict(X_test),labels=np.unique(y_test))
-  # records = records.append({'metrics': variabels,'train_acc':time_used,'val_acc': accuracy_score(y_test, loaded_model.predict(X_test)),'matrix':matrix},ignore_index=True)
+  ##final Model test accuracies
+  loaded_model = pickle.load(open(filename, 'rb'))
+  matrix = confusion_matrix(y_test,loaded_model.predict(X_test),labels=np.unique(y_test))
+  records = records.append({'metrics': variabels,'train_acc':time_used,'val_acc': accuracy_score(y_test, loaded_model.predict(X_test)),'matrix':matrix},ignore_index=True)
 
   records.to_csv(f'{OUTPUT}knn_cv4_records.csv')
   del X_train,X_validation,y_train,y_validation
